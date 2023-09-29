20842b1ba05552fe99ec615917567ff87d2fec04
AlanOGxD <alanosvaldo88@gmail.com>
2023-09-29 12:32:32 -0600

    Footer almost end

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -1,9 +1,14 @@
 /* Open when someone clicks on the span element */
+
 function openNav() {
+
+    document.getElementById("myNav").hidden = false;
     document.getElementById("myNav").style.left = "70%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
+
+    document.getElementById("myNav").hidden = true;
     document.getElementById("myNav").style.left = "100%";
 }
