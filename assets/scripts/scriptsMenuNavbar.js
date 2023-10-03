1a7b4030c0c854ec9dac0b7d0cf485dc3a37f8ae
Kalahazy <brandon_palacios11@hotmail.com>
2023-10-03 00:08:11 -0600

    fix: :iphone: Modify responsive design for form container

--- assets/scripts/scriptsMenuNavbar.js
+++ assets/scripts/scriptsMenuNavbar.js
@@ -1,24 +1,17 @@
-// referencia a la imagen de perfil y al menú de configuración
-const profileImg = document.getElementById('profile-img');
-const configMenu = document.getElementById('config-menu');
+document.addEventListener('DOMContentLoaded', () => {
+    // referencia a la imagen de perfil y al menú de configuración
+    const profileImg = document.getElementById('profile-img');
+    const configMenu = document.getElementById('config-menu');
 
-//evento a la imagen 
-profileImg.addEventListener('click', () => {
-    // visibilidad del menú de configuración 
-    if (configMenu.style.display === 'block') {
-        configMenu.style.display = 'none';
-    } else {
-        configMenu.style.display = 'block';
-    }
-});
-
-// Para cerrar el menú de configuración haciendo clicks afuerita 
-document.addEventListener('click', (e) => {
-    if (e.target !== profileImg && e.target !== configMenu) {
-        configMenu.style.display = 'none';
-    }
-
-});
+    //evento a la imagen 
+    profileImg.addEventListener('click', () => {
+        // visibilidad del menú de configuración 
+        if (configMenu.style.display === 'block') {
+            configMenu.style.display = 'none';
+        } else {
+            configMenu.style.display = 'block';
+        }
+    });
 
 
 
@@ -28,6 +21,7 @@ const menuElement = document.getElementById('config-menu'); //elemento del menuc
 if (isLoggedIn) {
     // Menucito para usuarios con sesión iniciada
     menuElement.innerHTML = `
+    "
     <ul>
     <li><a href="#">Detalles de Perfil</a></li> 
     <li><a href="#">Notificaciónes</a></li> 
@@ -36,26 +30,26 @@ if (isLoggedIn) {
     <li><a href="#">Logros</a></li> 
     <hr> 
     <li><a href="#">Cuenta y seguridad</a></li> 
-    <li><a href="/LinkGameIn/pages/AboutUs.html">Acerca de nosotros</a></li>
-    <li><a href="#">Términos y condiciones</a></li>
-    <li><a href="/LinkGameIn/pages/contactanos.html">Contáctanos</a></li>
+    <li><a href="/pages/AboutUs.html">Acerca de nosotros</a></li>
+    <li><a href="/pages/Terminosycondiciones.html">Términos y condiciones</a></li>
+    <li><a href="/pages/contactanos.html">Contáctanos</a></li>
 
     <li><a href="#">Cerrar sesión</a></li> 
-
-</ul>
-    `;
+    </ul>`
+    ;
 } else {
     // Menuito para usuarios sin sesión
-    menuElement.innerHTML = `
+    menuElement.innerHTML = 
         <ul>
         
-        <li><a href="/LinkGameIn/pages/AboutUs.html">Acerca de nosotros</a></li>
-        <li><a href="#">Términos y condiciones</a></li>
-        <li><a href="/LinkGameIn/pages/contactanos.html">Contáctanos</a></li>
+        <li><a href="/pages/AboutUs.html">Acerca de nosotros</a></li>
+        <li><a href="/pages/Terminosycondiciones.html">Términos y condiciones</a></li>
+        <li><a href="/pages/contactanos.html">Contáctanos</a></li>
         <hr>
-        <li><a href="/LinkGameIn/pages/registrarUsuario.html">Registrarse</a></li>
-        <li><a href="/LinkGameIn/pages/login.html">Iniciar sesión</a></li>
+        <li><a href="/pages/registrarUsuario.html">Registrarse</a></li>
+        <li><a href="/pages/login.html">Iniciar sesión</a></li>
         
     </ul>
-    `;
+    ;
 }
+})
