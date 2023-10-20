c7d190d678ac1731014960d7ce61aeabd149bafa
AlbertoSilvEn <aalbertosilvae@gmail.com>
2023-10-20 04:33:07 -0600

    Se agrega funcionalidad para guardar y eliminar publicacones del localStorage, falta agregar estilos y que se carge una imagen

--- assets/scripts/dashBoard.js
+++ assets/scripts/dashBoard.js
@@ -1,29 +1,70 @@
-document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+let contador = 1;
+const textarea = document.getElementById('post-text');
+const botonGuardar = document.getElementById('post-button');
+const contenedorTarjetas = document.getElementById('contenedorDePublicaciones');
 
-    // Obtén el contenido de la publicación del campo de texto
-    const contenidoPublicacion = document.querySelector('textarea').value;
+// Recupera las publicaciones guardadas desde el localStorage (si existen)
+function cargarPublicacionesGuardadas() {
+    const publicaciones = [];
+    for (let i = 0; i < localStorage.length; i++) {
+        const clave = localStorage.key(i);
 
-    // Crea una nueva publicación
-    const nuevaPublicacion = document.createElement('div');
-    nuevaPublicacion.classList.add('publicacion');
-    nuevaPublicacion.innerHTML = `
-        <p>${contenidoPublicacion}</p>
-    `;
+        if (clave.startsWith('publicacion')) {
+            const publicacionJSON = localStorage.getItem(clave);
+            const publicacion = JSON.parse(publicacionJSON);
+            publicaciones.push({ clave, ...publicacion });
+        }
+    }
 
-    // Agrega una nueva "publicación" al news feed
-    document.querySelector('#news-feed').appendChild(nuevaPublicacion);
-
-    //para borrar lo dell campo de texto
-    document.querySelector('textarea').value = '';
-});
-
-
-
-function abrirModal() {
-    document.getElementById('miModal').style.display = 'block';
+    // Mostrar hasta 10 publicaciones en el nuevo orden
+    for (let i = 0; i < Math.min(10, publicaciones.length); i++) {
+        mostrarPublicacion(publicaciones[i]);
+    }
 }
 
-function cerrarModal() {
-    document.getElementById('miModal').style.display = 'none';
+// Cargar publicaciones guardadas al cargar la página
+cargarPublicacionesGuardadas();
+
+// Agregar un evento de clic al botón para guardar el contenido en el localStorage y mostrarlo en una tarjeta
+botonGuardar.addEventListener('click', function() {
+    const contenido = textarea.value;
+
+    const publicacion = {
+        contenido: contenido,
+        fecha: new Date().toLocaleString(),
+    };
+
+    const claveUnica = 'publicacion' + contador;
+    contador++;
+
+    const publicacionJSON = JSON.stringify(publicacion);
+    localStorage.setItem(claveUnica, publicacionJSON);
+
+    mostrarPublicacion({ clave: claveUnica, ...publicacion });
+    textarea.value = ''; // Limpia el textarea después de guardar la publicación
+});
+
+// Función para mostrar una publicación en una tarjeta
+function mostrarPublicacion(publicacion) {
+    const nuevaTarjeta = document.createElement('div');
+    nuevaTarjeta.classList.add('tarjeta');
+    nuevaTarjeta.innerHTML = `
+        <p>${publicacion.contenido}</p>
+        <p>${publicacion.fecha}</p>
+        <button class="eliminar" data-clave="${publicacion.clave}">Eliminar</button>
+    `;
+
+    contenedorTarjetas.appendChild(nuevaTarjeta);
+
+    // Agregar un evento de clic al botón "Eliminar"
+    const botonEliminar = nuevaTarjeta.querySelector('.eliminar');
+    botonEliminar.addEventListener('click', function() {
+        eliminarPublicacion(publicacion.clave);
+        contenedorTarjetas.removeChild(nuevaTarjeta);
+    });
+}
+
+// Función para eliminar una publicación del localStorage
+function eliminarPublicacion(clave) {
+    localStorage.removeItem(clave);
 }
